da19fb4881946a4ff38f0736f07084f0690f31da
Rahmat Syarif Azhari <zack@upi.edu>
2022-04-01 14:33:11 +0700

    Update splash.dart

--- lib/splash.dart
+++ lib/splash.dart
@@ -3,27 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:iku_application/login.dart';
 
-// class Splash extends StatelessWidget {
-//   const Splash({Key? key, required MaterialColor backgroundColor, nextScreen})
-//       : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'IKU',
-//       theme:
-//           ThemeData(scaffoldBackgroundColor: Color.fromARGB(255, 240, 165, 0)),
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [Image.asset('assets/images/splashikuupi.png')]),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
