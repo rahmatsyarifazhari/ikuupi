e6b6985f2a1bf5780af543787ae0eb0bbc739bf1
makhizu <79908795+makhizu@users.noreply.github.com>
2022-06-02 07:27:28 +0700

    Update FIP.dart

--- lib/FIP.dart
+++ lib/FIP.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'IKU.dart';
 
 void main() {
   runApp(const FIP());
@@ -12,141 +13,152 @@ class FIP extends StatelessWidget {
     return MaterialApp(
       title: 'GRID',
       home: Scaffold(
-          // appBar: AppBar(
-          //   title: const Text('Hello'),
-          // ),
-          body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              fit: BoxFit.cover,
-              image: const AssetImage("images/isola.jpg"),
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.2), BlendMode.dstATop),
-            )),
+          appBar: AppBar(
+            leading: GestureDetector(
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            centerTitle: true,
+            title: const Text('FIP'),
           ),
-          Center(
-            child: GridView(
-                padding: const EdgeInsets.all(20),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        // border: Border.all(),
-                        image: const DecorationImage(
-                            image: AssetImage("images/UPI.png"))),
-                    padding: const EdgeInsets.all(14),
-                    margin: const EdgeInsets.all(20),
-                    alignment: Alignment.bottomCenter,
-                    child: const Text(
-                      'Bimbingan dan Konseling',
-                      style: TextStyle(backgroundColor: Colors.white),
+          body: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: const AssetImage("images/isola.jpg"),
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                )),
+              ),
+              Center(
+                child: GridView(
+                    padding: const EdgeInsets.all(20),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
                     ),
-                  ),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Administrasi Pendidikan',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Pendidikan Luar Sekolah',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Pendidikan Luar Biasa',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Teknologi Pendidikan',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'PGSD Bumi Siliwangi',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'PGPAUD Bumi Siliwangi',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Psikologi',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                  Container(
-                      decoration: BoxDecoration(
-                          // border: Border.all(),
-                          image: const DecorationImage(
-                              image: AssetImage("images/UPI.png"))),
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.all(20),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text(
-                        'Perpustakaan dan Informasi',
-                        style: TextStyle(backgroundColor: Colors.white),
-                      )),
-                ]),
-          )
-        ],
-      )),
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/UPI.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        child: const Text(
+                          'Bimbingan dan Konseling',
+                          style: TextStyle(backgroundColor: Colors.white),
+                        ),
+                      ),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Administrasi Pendidikan',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Pendidikan Luar Sekolah',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Pendidikan Luar Biasa',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Teknologi Pendidikan',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'PGSD Bumi Siliwangi',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'PGPAUD Bumi Siliwangi',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Psikologi',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              // border: Border.all(),
+                              image: const DecorationImage(
+                                  image: AssetImage("images/UPI.png"))),
+                          padding: const EdgeInsets.all(14),
+                          margin: const EdgeInsets.all(20),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Perpustakaan dan Informasi',
+                            style: TextStyle(backgroundColor: Colors.white),
+                          )),
+                    ]),
+              )
+            ],
+          )),
     );
   }
 }
