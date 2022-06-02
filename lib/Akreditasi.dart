821d759f801b4f285b3329f085b763b1de7bb4db
adeariyan <79646808+adeariyan@users.noreply.github.com>
2022-06-02 12:22:52 +0700

    Add files via upload

--- lib/Akreditasi.dart
+++ lib/Akreditasi.dart
@@ -27,6 +27,51 @@ class Akreditasi extends StatelessWidget {
                   decoration: BoxDecoration(border: Border.all()),
                   padding: EdgeInsets.all(14),
                   child: Text('FPIPS')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FIP')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPBS')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPSD')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPTK')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPOK')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPEB')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D CIBIRU')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D SUMEDANG')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D TASIKMALAYA')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D PURWAKARTA')),
+               Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D SERANG')),
+
             ]),
       )),
     );
