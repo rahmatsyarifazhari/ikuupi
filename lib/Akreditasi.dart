90245f5b0b29e1e10566643650efe6aca6a2b3a4
makhizu <79908795+makhizu@users.noreply.github.com>
2022-06-03 15:49:31 +0700

    update akreditasi

--- lib/Akreditasi.dart
+++ lib/Akreditasi.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:iku_application/Home.dart';
 
 void main() {
   runApp(const Akreditasi());
@@ -12,111 +14,129 @@ class Akreditasi extends StatelessWidget {
     return MaterialApp(
       title: 'Akreditasi',
       home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: HexColor("#E45826"),
+            leading: InkWell(
+              child: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return Home();
+                }));
+              },
+            ),
+            centerTitle: true,
+            title: const Text('Akreditasi'),
+          ),
+          // appBar: ,
           body: Stack(children: [
-        Container(
-          height: double.infinity,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            fit: BoxFit.cover,
-            image: const AssetImage("images/isola.jpg"),
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.dstATop),
-          )),
-        ),
-        ListView(padding: const EdgeInsets.all(20), children: [
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Column(children: [
-                Text(
-                  "FPMIPA",
-                  style: TextStyle(backgroundColor: Colors.orange[400]),
-                ),
-                DataTable(
-                  columns: <DataColumn>[
-                    DataColumn(label: Text("No")),
-                    DataColumn(label: Text("Nama Prodi")),
-                    DataColumn(label: Text("Jenjang")),
-                    DataColumn(label: Text("Akreditasi")),
-                  ],
-                  rows: <DataRow>[
-                    DataRow(
-                      cells: <DataCell>[
-                        DataCell(Text("1")),
-                        DataCell(Text("Ilmu Komputer")),
-                        DataCell(Text("S-1")),
-                        DataCell(Text("A")),
+            Container(
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                fit: BoxFit.cover,
+                image: const AssetImage("images/isola.jpg"),
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.2), BlendMode.dstATop),
+              )),
+            ),
+            ListView(padding: const EdgeInsets.all(20), children: [
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Column(children: [
+                    Text(
+                      "FPMIPA",
+                      style: TextStyle(backgroundColor: Colors.orange[400]),
+                    ),
+                    DataTable(
+                      columns: <DataColumn>[
+                        DataColumn(label: Text("No")),
+                        DataColumn(label: Text("Nama Prodi")),
+                        DataColumn(label: Text("Jenjang")),
+                        DataColumn(label: Text("Akreditasi")),
+                      ],
+                      rows: <DataRow>[
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text("1")),
+                            DataCell(Text("Ilmu Komputer")),
+                            DataCell(Text("S-1")),
+                            DataCell(Text("A")),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text("2")),
+                            DataCell(Text("Kimia")),
+                            DataCell(Text("S-1")),
+                            DataCell(Text("A")),
+                          ],
+                        ),
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text("3")),
+                            DataCell(Text("Biologi")),
+                            DataCell(Text("S-1")),
+                            DataCell(Text("A")),
+                          ],
+                        ),
                       ],
                     ),
-                    DataRow(
-                      cells: <DataCell>[
-                        DataCell(Text("2")),
-                        DataCell(Text("Kimia")),
-                        DataCell(Text("S-1")),
-                        DataCell(Text("A")),
-                      ],
-                    ),
-                    DataRow(
-                      cells: <DataCell>[
-                        DataCell(Text("3")),
-                        DataCell(Text("Biologi")),
-                        DataCell(Text("S-1")),
-                        DataCell(Text("A")),
-                      ],
-                    ),
-                  ],
-                ),
-              ])),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPIPS')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FIP')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPBS')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPSD')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPTK')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPOK')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('FPEB')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('K.D CIBIRU')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('K.D SUMEDANG')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('K.D TASIKMALAYA')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('K.D PURWAKARTA')),
-          Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Text('K.D SERANG')),
-        ]),
-      ])),
+                  ])),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPIPS')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FIP')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPBS')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPSD')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPTK')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPOK')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('FPEB')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D CIBIRU')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D SUMEDANG')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D TASIKMALAYA')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D PURWAKARTA')),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  padding: EdgeInsets.all(14),
+                  child: Text('K.D SERANG')),
+            ]),
+          ])),
     );
   }
 }
