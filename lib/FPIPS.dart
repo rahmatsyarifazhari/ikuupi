88bf05ff7808b556c8879beaee7e8177f05dcb49
adeariyan <79646808+adeariyan@users.noreply.github.com>
2022-06-14 23:49:35 +0700

    Add files via upload

--- lib/FPIPS.dart
+++ lib/FPIPS.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'IKU.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+
 void main() {
   runApp(const FPIPS());
 }
@@ -14,9 +15,6 @@ class FPIPS extends StatelessWidget {
     return MaterialApp(
       title: 'GRID',
       home: Scaffold(
-          // appBar: AppBar(
-          //   title: const Text('Hello'),
-          // ),
           appBar: AppBar(
             leading: GestureDetector(
               child: Icon(
@@ -32,123 +30,202 @@ class FPIPS extends StatelessWidget {
             title: const Text('FPIPS'),
           ),
           body: Stack(
-            children: [
-              Container(
-                height: double.infinity,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: const AssetImage("images/isola.jpg"),
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.2), BlendMode.dstATop),
-                )),
-              ),
-              Center(
-                child: GridView(
-                    padding: const EdgeInsets.all(20),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
+        children: [
+          Container(
+            height: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              fit: BoxFit.cover,
+              image: const AssetImage("images/isola.jpg"),
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.dstATop),
+            )),
+          ),
+          Center(
+            child: GridView(
+                padding: const EdgeInsets.all(20),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                children: [
+                  InkWell(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          // border: Border.all(),
+                          image: const DecorationImage(
+                              image: AssetImage("images/PKN.png"))),
+                      padding: const EdgeInsets.all(14),
+                      margin: const EdgeInsets.all(20),
+                      alignment: Alignment.bottomCenter,
                     ),
-                    children: [
-                      Container(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
                         decoration: BoxDecoration(
                             // border: Border.all(),
                             image: const DecorationImage(
-                                image: AssetImage("images/PKN.png"))),
+                                image: AssetImage("images/SEJARAH.png"))),
                         padding: const EdgeInsets.all(14),
                         margin: const EdgeInsets.all(20),
                         alignment: Alignment.bottomCenter,
-                      ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/SEJARAH.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                          ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/GEOGRAFI.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                          ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/UPI.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                          child: const Text(
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/GEOGRAFI.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                   InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/UPI.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.center,
+                        child: const Text(
                             'Pendidikan Agama Islam',
-                            style: TextStyle(fontWeight: FontWeight.bold, backgroundColor: Colors.white),
+                            style: TextStyle(backgroundColor: Colors.white),
                           )
-                          ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/MRL.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                          ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/MPP.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                         ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/MIKA.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                         ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/IPS.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                         ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/ILMUKOMUNIKASI.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                         ),
-                      Container(
-                          decoration: BoxDecoration(
-                              // border: Border.all(),
-                              image: const DecorationImage(
-                                  image: AssetImage("images/SOSIOLOGI.png"))),
-                          padding: const EdgeInsets.all(14),
-                          margin: const EdgeInsets.all(20),
-                          alignment: Alignment.bottomCenter,
-                         ),
-                    ]),
-              )
-            ],
-          )),
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                   InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/MRL.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                   InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/MPP.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/MIKA.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/IPS.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/ILMUKOMUNIKASI.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(),
+                            image: const DecorationImage(
+                                image: AssetImage("images/SOSIOLOGI.png"))),
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.all(20),
+                        alignment: Alignment.bottomCenter,
+                        ),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return FPIPS();
+                      }));
+                    },
+                  ),
+                ]),
+          )
+        ],
+      )),
     );
   }
 }
